e65f62f65fc67dbb8783f5895b13dcdb38925471
Peacecole <onyangocollince@gmail.com>
2023-03-16 10:55:30 -0700

    Compiler

---
+++ 0x00-hello_world/main-1.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main- age of a person
+ * Asks a person their age and aloows for response
+ * Return: return 0
+ */
+
+int main(void)
+{
+	int age;
+
+	printf("What is your age?");
+	scanf("%d", &age);
+
+	printf("I am %d\n", age);
+
+		return (0);
+}
