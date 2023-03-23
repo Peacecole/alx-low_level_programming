d5b210d4c480861cac22d863fce0c8036aede151
Peacecole <onyangocollince@gmail.com>
2023-03-23 10:28:24 -0700

    A program that multiplies two integers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - Return the product of two numbers
+ * @num1: first integer type
+ * @num2: second integer type
+ * Return: product of two numbers
+ */
+int mul(int num1, int num2)
+{
+	return (num1 * num2);
+}
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * mul - Return the product of two numbers
+ * @num1, num2: integer type letter
+ * Return: product of two numbers
+ */
+int mul(int num1, int num2)
+{
+	int prod;
+
+	prod = num1 * num2;
+	_putchar("%d", prod)
+		_putchar("\n")
+		
+		return (0);
+}
