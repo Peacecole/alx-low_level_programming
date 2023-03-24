5d7665d872f569f3714fc7e2fe2d36fb366f5f82
Peacecole <onyangocollince@gmail.com>
2023-03-24 06:29:37 -0700

    A code that test a function

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * main - entry point
+ * tests function that prints if integer is positive or negative
+ * Return: always 0
+ */
+int main(void)
+{
+
+	int x;
+
+	x = 0;
+
+	positive_or_negative(x);
+
+	return (0);
+}
