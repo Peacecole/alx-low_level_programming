05866154a356d60b2e324e382a65c5271c10dd14
Peacecole <onyangocollince@gmail.com>
2023-03-24 07:30:57 -0700

    A program that creates an ending loop

---
+++ 0x03-debugging/1-main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - causes an ending loop
+ * Return: always 0
+ *
+ */
+int main(void)
+{
+	int i;
+
+	printf("An ending loop :(\n");
+
+	i = 0;
+
+	/*while(i < 10)*/
+	/*{*/
+	/*putchar(i);*/
+	/*}*/
+
+	printf("Infinite loop avoided!\\o/\n");
+
+	return (0);
+}
