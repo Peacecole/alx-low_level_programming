ea6bfe23fe93770fe86d5854b3e9d11187983906
Peacecole <onyangocollince@gmail.com>
2023-03-30 11:43:47 -0700

    function that changes lowercase letters to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * string_toupper - lowercase letters of a string to uppercase function
+ * @str: input string.
+ * Return: the pointer to dest.
+ */
+char *string_toupper(char *str)
+{
+
+	int i = 0;
+
+	while (*(str + i) != '\0')
+	{
+
+		if ((*(str + i) >= 97) && (*(str + i) <= 122))
+			*(str + i) = *(str + i) - 32;
+		i++;
+	}
+
+	return (str);
+}
+
