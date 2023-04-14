2378e5276cf77952d6b25fd036ef078b5d210a63
Peacecole <onyangocollince@gmail.com>
2023-04-14 08:34:34 -0700

    header file update

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -6,4 +6,7 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 int _putchar(char c);
+int digit_test(char *str);
+int _strlen(char *str);
+void errors(void);
 #endif
