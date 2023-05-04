63281f1c40c3070e83bbf92118570315106e9a41
Peacecole <onyangocollince@gmail.com>
2023-05-04 22:10:30 +0000

    task 2.10 - a function that returns the value of a bit at a given index.

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * set_bit - sets the value of a bit to 1 at a given index
+ * @n: pointer to a the number whose bit is to be set to 1
+ * @index: index to the bit to be set to 1
+ * Return: integer
+ */
+int get_bit(unsigned long int n, unsigned int index)
+{
+	int bit;
+
+	if (index > 63)
+		return (-1);
+
+	bit = (n >> index) & 1;
+
+	return (bit);
+}
